838cb307a49df3d58a936eb07c58c6627b59eda1
samComeIt <sanhae1996@gmail.com>
2020-07-30 15:20:37 +0900

    update method for selected id

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -7,11 +7,9 @@ from .forms import *
 
 def index(request):
     tasks = Task.objects.all()
-
-    
     form = TaskForm()
 
-    if request.method =="POST":
+    if request.method =='POST':
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
@@ -20,4 +18,16 @@ def index(request):
     context = {'tasks':tasks, 'form':form}
     return render(request, 'tasks/list.html', context)
 
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
 
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form':form}
+
+    
+    return render(request, 'tasks/update_task.html', context)
